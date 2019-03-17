b270340c3f8accbedcdaa7457deff354ed57be1a
Eric Ross <epr@evross.com>
2019-03-17 12:38:05 -0700

    Added a component called settings

--- section7/src/router.js
+++ section7/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Settings from "./views/Settings.vue"
 
 Vue.use(Router);
 
@@ -21,6 +22,16 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Settings.vue")
+    },
+
   ]
 });
---
+++ section7/src/views/Settings.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="settings">
+    <h1>This is a settings page</h1>
+</div>
+</template>
+
+<script>
+
+</script>
+
